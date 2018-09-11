d1ee9d758b9ccb77c626c2926d893908947b365e
kshameed7@gmail.com <kshameed7@gmail.com>
2018-09-11 23:38:36 +0530

    Server API with .net framework 4.7

---
+++ Source/KidzmileServer/Kidzmile/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Kidzmile.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
