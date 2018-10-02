e012c53e91e7905e68be1b0ecebc584b6d99db89
kshameed7@gmail.com <kshameed7@gmail.com>
2018-10-02 18:10:38 +0530

    Adding Router module
    Adding Router module  to maintain separate module for routing purposes only

---
+++ Source/KidzmileUI/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component'
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './Login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+// The last route is the empty path route. This specifies
+// the route to redirect to if the client side path is empty.
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {  },
+  { path: '', redirectTo: '/list', pathMatch: 'full' }
+];
+
+// Pass the configured routes to the forRoot() method
+// to let the angular router know about our routes
+// Export the imported RouterModule so router directives
+// are available to the module that imports this AppRoutingModule
+@NgModule({
+  imports: [ RouterModule.forRoot(appRoutes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- Source/KidzmileUI/src/app/app.module.ts
+++ Source/KidzmileUI/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component'
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './Login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +14,8 @@ import { LoginComponent } from './Login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
