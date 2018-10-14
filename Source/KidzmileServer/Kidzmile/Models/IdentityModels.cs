74c27b0f5630360efb3ac2db9daf550964a294bb
kshameed7@gmail.com <kshameed7@gmail.com>
2018-10-14 17:01:03 +0530

    Migrating Database
    Adding fname,lastname,createdts,updatedts properties to the database table

--- Source/KidzmileServer/Kidzmile/Models/IdentityModels.cs
+++ Source/KidzmileServer/Kidzmile/Models/IdentityModels.cs
@@ -3,12 +3,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using System.Data.Entity;
+using System;
 
 namespace Kidzmile.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -16,6 +22,8 @@ namespace Kidzmile.Models
             // Add custom user claims here
             return userIdentity;
         }
+        public DateTime CreatedTS { get; set; }
+        public DateTime UpdatedTS { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -28,6 +36,26 @@ namespace Kidzmile.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //AspNetUsers -> User
+            modelBuilder.Entity<ApplicationUser>()
+                .ToTable("User");
+            //AspNetRoles -> Role
+            modelBuilder.Entity<IdentityRole>()
+                .ToTable("Role");
+            //AspNetUserRoles -> UserRole
+            modelBuilder.Entity<IdentityUserRole>()
+                .ToTable("UserRole");
+            //AspNetUserClaims -> UserClaim
+            modelBuilder.Entity<IdentityUserClaim>()
+                .ToTable("UserClaim");
+            //AspNetUserLogins -> UserLogin
+            modelBuilder.Entity<IdentityUserLogin>()
+                .ToTable("UserLogin");
         }
     }
 }
