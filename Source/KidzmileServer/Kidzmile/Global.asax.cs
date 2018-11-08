a925c44100b823343290b24c5915434529f24467
kshameed7@gmail.com <kshameed7@gmail.com>
2018-11-08 12:49:05 +0530

    handling preflight

--- Source/KidzmileServer/Kidzmile/Global.asax.cs
+++ Source/KidzmileServer/Kidzmile/Global.asax.cs
@@ -13,6 +13,7 @@ namespace Kidzmile
     {
         protected void Application_Start()
         {
+            
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
@@ -20,6 +21,21 @@ namespace Kidzmile
             UnityConfig.RegisterComponents();
             BundleConfig.RegisterBundles(BundleTable.Bundles);
            
+        }
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {
+            // Preflight request comes with HttpMethod OPTIONS
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Cache-Control", "no-cache");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
+                // The following line solves the error message
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+                // If any http headers are shown in preflight error in browser console add them below
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, Pragma, Cache-Control, Authorization,No-Auth ");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                HttpContext.Current.Response.End();
+            }
         }
     }
 }
